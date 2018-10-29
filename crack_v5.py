80079774e4ca7a7b06ee301bc4639a9a15c2c7cc
Garamda <Starever222@gmail.com>
2018-10-30 00:56:40 +0900

    Update crack_v5.py

--- crack_v5.py
+++ crack_v5.py
@@ -246,7 +246,7 @@ for i in range(0, frames_count):
                     draw(image)
                     boximg_path = "/home/starever222/SPARK/SPARK/public/images/"+filename+"/%d.jpg" % counting
                     image.save(filename=boximg_path)
-
+             break;
         counting += 6
         if(counting>frames_count): 
             isBreak = 1;
